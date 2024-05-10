0840b8c6991c341f14eb1e269f2ffa7198856beb
Nazemrap <corentin.damas@gmail.com>
2024-05-10 08:49:13 +0200

    success and fail redirect (4)

--- src/app/(root)/api/checkout/route.ts
+++ src/app/(root)/api/checkout/route.ts
@@ -21,7 +21,7 @@ export const POST = async (request: any) => {
   
   const getBaseUrl = () => {
     if (process.env.NEXT_PUBLIC_VERCEL_ENV === "production")
-      return `https://${process.env.VERCEL_URL}`;
+      return `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`;
     if (process.env.NEXT_PUBLIC_VERCEL_ENV === "preview")
       return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
     return "http://localhost:3000";
